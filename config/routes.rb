c7dd18bfdb02a3d1a5e34954208bf346be3eff4d
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-15 11:12:23 +0200

    e

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants
   devise_for :users
-  root to: 'pages#home'
+  root to: 'restaurants#index'
+  resources :restaurants, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
